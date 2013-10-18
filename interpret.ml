ee4e7248f4d71cc754eeefd5bb5bf3fb9692454d
Chris D'Angelo <cd2665@columbia.edu>
2013-10-18 18:03:46 -0400

    commented entry point for interpreter

--- interpret.ml
+++ interpret.ml
@@ -16,6 +16,8 @@ open Ast
  * ADT dictionary (key/value). The value that will be returned for
  * a valid key will be of the type defined in the struct...end
  *
+ * the module name (in this case "NameMap") must be capitalized
+ *
  * It appears as thought this NameMap is no different then a normal
  * map where string is the key and the value is yet to be defined
  *
@@ -31,37 +33,39 @@ end)
  *)
 exception ReturnException of int * int NameMap.t
 
-(* Main entry point: run a program *)
-
+(* 
+ * run function that is the entry point for the program
+ * microc will use this handle to start the interpreter
+ * however the actual entry point is way down below
+ * which calls the first function main
+ *)
 let run (vars, funcs) =
   (* Put function declarations in a symbol table *)
   let func_decls = List.fold_left
-  	  (*
-  	   * OCaml reminder:
-  	   * List.fold_left f a [b1; ...; bn]
-  	   * will produce f (...(f (f a b1) b2)...) bn
-  	   *
-  	   * so in this case we are working with
-	   * List.fold_left (function to add the functions to a NameMap) (empty NameMap) (list of functions)
+      (*
+       * OCaml reminder:
+       * List.fold_left f a [b1; ...; bn]
+       * will produce f (...(f (f a b1) b2)...) bn
        *
-  	   * f   = function that takes in two arguments a (funcs) and b (fdecl)
-  	   *       which is items from the list of functions declared
+       * so in this case we are working with
+       * List.fold_left (function to add the functions to a NameMap) (empty NameMap) (list of functions)
+       *
+       * f   = function that takes in two arguments a (funcs) and b (fdecl)
+       *       which is items from the list of functions declared
        * a   = growing Map. NameMap.add always takes a map argument and adds its key/value
        *       and returns a map. In fold_left this will be fed into the next iteration
        *       so NameMap.add will have the "a" continually updating with the new map
        * b_n = will an iteration through the funcs list of function declarations
        *
        * example:
-       *
        * (NameMap.add (Key: "main") (Value: <<main declaration info>>) current_map)
-       * 
        * previous naming was pretty confusing and has been simplified below
        *
-  	   *)
-
-      (fun growing_map function_definition_n -> NameMap.add function_definition_n.fname function_definition_n growing_map)
-      NameMap.empty funcs
-  in
+       * The end result is that we now have function declarations in a symbol
+       * table
+       *)
+      (fun fmap fd_n -> NameMap.add fd_n.fname fd_n fmap) NameMap.empty funcs
+  in (* end of let func_decls = List.fold_left *)
 
   (* Invoke a function and return an updated global symbol table *)
   let rec call fdecl actuals globals =
@@ -118,7 +122,7 @@ let run (vars, funcs) =
 	    let globals = call fdecl actuals globals
 	    in 0, (locals, globals)
 	  with ReturnException(v, globals) -> v, (locals, globals)
-    in
+    in (* end of let rec eval env = function *)
 
     (* Execute a statement and return an updated environment *)
     let rec exec env = function
@@ -145,7 +149,7 @@ let run (vars, funcs) =
       | Return(e) ->
 	  let v, (locals, globals) = eval env e in
 	  raise (ReturnException(v, globals))
-    in
+    in (* end of let rec exec env = function *)
 
     (* Enter the function: bind actual values to formal arguments *)
     let locals =
@@ -154,17 +158,53 @@ let run (vars, funcs) =
 	  NameMap.empty fdecl.formals actuals
       with Invalid_argument(_) ->
 	raise (Failure ("wrong number of arguments passed to " ^ fdecl.fname))
-    in
+    in (* end of let locals = *)
+
+
     (* Initialize local variables to 0 *)
     let locals = List.fold_left
 	(fun locals local -> NameMap.add local 0 locals) locals fdecl.locals
-    in
+    in (* end of let locals = List.fold_left *)
+
     (* Execute each statement in sequence, return updated global symbol table *)
     snd (List.fold_left exec (locals, globals) fdecl.body)
 
   (* Run a program: initialize global variables to 0, find and run "main" *)
-  in let globals = List.fold_left
-      (fun globals vdecl -> NameMap.add vdecl 0 globals) NameMap.empty vars
-  in try
-    call (NameMap.find "main" func_decls) [] globals
-  with Not_found -> raise (Failure ("did not find the main() function"))
+  in (* end of let rec call fdecl actuals globals = *)
+
+
+
+  
+  
+  let globals = List.fold_left
+     (*
+      * List.fold_left f a [b1; ...;bn]
+      * f (... (f (f a b1) b2) ...) bn
+      *
+      * This method of ingesting the global variable declarions is essentially
+      * the same as the above method of ingesting the function declarations
+      *
+      * Starting off with an emtpy map and the vars list passed in from the
+      * original function call we work from left to right in the list
+      * adding each variable name with a value of 0 to the map.
+      * eventually saving the results in globals.
+      *
+      * (fun gmap vd) renamed for clearity
+      *)
+     (fun gmap vd -> NameMap.add vd 0 gmap) NameMap.empty vars
+  in (* end of let globals = List.fold_left *)
+     try
+     (*
+      * NameMap.find will lookup the key "main" in the map func_decls
+      * And will return the func_decls if it si found
+      *
+      * if it is not found .find should return an exception
+      * that will be caught and matched by with Not_found.
+      * We raise our own exception named "Failure" with a message
+      *
+      * call takes the arguments fdecl actulas and globals
+      * the main() in microc has no arguments/actuals
+      * the globals we pass in is the result of the above
+      *)
+     call (NameMap.find "main" func_decls) [] globals
+     with Not_found -> raise (Failure ("did not find the main() function"))
