1e79ce7c05677a8d7c537a5fd657b9e4908412a2
Chris D'Angelo <cd2665@columbia.edu>
2013-11-08 02:46:05 -0500

    dont loosely commenting execute

--- execute.ml
+++ execute.ml
@@ -13,11 +13,31 @@ open Bytecode
    ...
    Arg n *)
 
+(*
+ * Example of prog:
+ * 
+ *  prog: Bytecode.prog =
+ *   {num_globals = 1;
+ *    text =
+ *     [|Jsr 2; Hlt; Ent 1; Lit 42; Sfp 1; Drp; Lfp 1; Jsr 15; Str 0; Drp;
+ *      Lod 0; Jsr (-1); Drp; Lit 0; Rts 0; Ent 1; Lit 1; Sfp 1; Drp; Lfp (-2);
+ *      Lfp 1; Bin Add; Rts 1; Lit 0; Rts 1|]}
+ *)
+
 let execute_prog prog =
+  (*
+   * OCaml Reminder:
+   * Array.make 1024 0 will create an array of 1024 zeros
+   *)
   let stack = Array.make 1024 0
   and globals = Array.make prog.num_globals 0 in
 
   let rec exec fp sp pc = match prog.text.(pc) with
+    (*
+     * Example run of the above prog
+     * step 3)
+     * Lit 42 -> stack.(3) <- 42 ; exec 1 (3+1) (3+1)
+     *)
     Lit i  -> stack.(sp) <- i ; exec fp (sp+1) (pc+1)
   | Drp    -> exec fp (sp-1) (pc+1)
   | Bin op -> let op1 = stack.(sp-2) and op2 = stack.(sp-1) in     
@@ -34,12 +54,45 @@ let execute_prog prog =
       | Greater -> boolean (op1 >  op2)
       | Geq     -> boolean (op1 >= op2)) ;
       exec fp (sp-1) (pc+1)
+  (*
+   * Ocaml Reminder:
+   * Arrays are mutable and stack.(sp) <- stack.(fp+i) is setting stack.(sp)
+   * with stack.(fp+i)
+   *)
   | Lod i   -> stack.(sp)   <- globals.(i)  ; exec fp (sp+1) (pc+1)
   | Str i   -> globals.(i)  <- stack.(sp-1) ; exec fp sp     (pc+1)
   | Lfp i   -> stack.(sp)   <- stack.(fp+i) ; exec fp (sp+1) (pc+1)
+  (*
+   * Example run of the above prog
+   * step 4)
+   * Sfp 1 -> stack.(2) <- stack.(4-1) ; exec 1 4 (4+1)
+   *)
   | Sfp i   -> stack.(fp+i) <- stack.(sp-1) ; exec fp sp     (pc+1)
   | Jsr(-1) -> print_endline (string_of_int stack.(sp-1)) ; exec fp sp (pc+1)
+  (*
+   * Example run of the above prog
+   * step 1)
+   * Jsr 2 -> stack.(0) <- 0+1 ; exec 0 (0+1) 2
+   *
+   * Unconditional jump to instruction 2
+   * Store on the top of the stack program counter + 1 (next source code)
+   * next: move onto the next instruction
+   * keep the frame pointer the same
+   * iterate the stack pointer
+   * change the program counter to the instruction we're currently on
+   *)
   | Jsr i   -> stack.(sp)   <- pc + 1       ; exec fp (sp+1) i
+  (*
+   * Example run of the above prog
+   * step 2)
+   * Ent 1 -> stack.(1) <- 0 ; exec 1 (1+1+1) 3
+   *
+   * Store the old frame pointer where the stack pointer currently is (just past the
+   * end of the last activation record).
+   * Next: move onto the next instruction (Jsr 2)+1
+   * execute the next instruction with the current SP as the new FP
+   * and with the current SP+sizeOfStackSpaceWeneed+OnePastEnd
+   *)
   | Ent i   -> stack.(sp)   <- fp           ; exec sp (sp+i+1) (pc+1)
   | Rts i   -> let new_fp = stack.(fp) and new_pc = stack.(fp-1) in
                stack.(fp-i-1) <- stack.(sp-1) ; exec new_fp (fp-i) new_pc
--- hello2.mc
+++ hello2.mc
@@ -23,32 +23,35 @@
  * 
  *   Byte Code Printed using bytecode.ml "string_of_prog" with comments:
  *
- *	 1 global variables   # first just print the number of globals
- *   0 Jsr 2 			  # Call function absolute address 2
- *   1 Hlt				  
+ *	 1 global variables   # First just print the number of globals
+ *
+ *   0 Jsr 2 			  # Call function at address 2 (unconditional jump to addr)
+ *   1 Hlt				  # Stop program
+ *
  *   2 Ent 1    		  # Main begins. Allocate Stack space for 1 local.
  *   3 Lit 42			  # Literal 42
- *   4 Sfp 1              # Store in location 1 relative to FP
+ *   4 Sfp 1              # Store in location 1 relative to Frame Pointer
  *   5 Drp				  # Discard value
- *   6 Lfp 1              # Load Value from position FP+1
+ *   6 Lfp 1              # Load Value from position FP+1 (b's location)
  *   7 Jsr 15             # Call function from absolute address 15
- *   8 Str 0
- *   9 Drp
+ *   8 Str 0			  # Store global variable ref 0
+ *   9 Drp				  # Discard value
  *   10 Lod 0             # Fetch Global Variable 0
  *   11 Jsr -1            # print
- *   12 Drp				  #
+ *   12 Drp				  # Discard value
  *   13 Lit 0			  # default = return 0
  *   14 Rts 0			  # Restore File Pointer
- *   15 Ent 1
- *   16 Lit 1
- *   17 Sfp 1
- *   18 Drp
- *   19 Lfp -2
- *   20 Lfp 1
- *   21 Add
- *   22 Rts 1
- *   23 Lit 0
- *   24 Rts 1
+ *
+ *   15 Ent 1             # Inc begins. Allocate stack space for 1 local
+ *   16 Lit 1             # Literal 1
+ *   17 Sfp 1             # Store in location 1 relative to Frame Pointer
+ *   18 Drp				  # Discard value
+ *   19 Lfp -2            # Load Value from position FP-2
+ *   20 Lfp 1             # Load Value from position FP+1 (y's location)
+ *   21 Add               # Add 
+ *   22 Rts 1             # Restore FP, SP, consume formals, push result
+ *   23 Lit 0             # push literal
+ *   24 Rts 1             # Restore FP, SP, consume formals, push result
  */
 
 int a;
